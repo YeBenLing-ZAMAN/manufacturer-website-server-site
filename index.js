82eb8ddb84bdc632ff62ff5b6ef54e871738cf9c
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-26 07:02:06 +0600

    mongodb is connected successfully

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -13,6 +14,30 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.takrq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db('tools').collection('products');
+
+
+    app.get('/tools', async (req, res) => {
+      const tools = await toolsCollection.find().toArray();
+      res.send(tools);
+
+  })
+
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

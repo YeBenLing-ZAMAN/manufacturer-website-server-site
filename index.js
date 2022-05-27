8e8debef05554b53c11ebc6669f64dd45e77a9c7
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-27 09:15:10 +0600

    jwt token verfication added

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+  });
+  // next();
+}
+
 async function run() {
   try {
     await client.connect();
@@ -27,7 +45,7 @@ async function run() {
     const bookingCollection = client.db('tools').collection('bookingProducts');
     const reviewCollection = client.db('tools').collection('reviews');
     const usersCollection = client.db('tools').collection('users');
-    
+
     app.get('/tools', async (req, res) => {
       const tools = await toolsCollection.find().toArray();
       res.send(tools);
@@ -49,10 +67,10 @@ async function run() {
       return res.send({ success: true, result });
     })
 
-    app.get('/booking', async (req, res) => {
+    app.get('/booking', verifyJWT, async (req, res) => {
       const email = req.query.user;
-      /*  const authorization = req.headers.authorization;
-       console.log('auth header: ',authorization); */
+       const authorization = req.headers.authorization;
+       console.log('auth header: ',authorization);
       //const decodedEmail = req.decoded.email;
       //if (patient === decodedEmail) {
       const query = { useremail: email };
